3cd103bc372582e173a67f68e7dc5e9ffa5196f3
Atharv-Pathak <88093066+Atharv-Pathak@users.noreply.github.com>
2021-11-20 18:19:45 +0530

    Update song.js

--- server/routes/api/media/song.js
+++ server/routes/api/media/song.js
@@ -10,11 +10,15 @@ module.exports = function getSong(req, res, next) {
     const html = yield request(
       `https://mp3.zing.vn/bai-hat/${name}/${id}.html`
     );
-    const regex = /key=.{33}/; // get the resouce url
+        const regex = /key=.{33}/; // get the resouce url
 
     const match = html.match(regex);
 
     if (!match) throw new Error("can't find the resource URL");
+
+    const [matchUrl] = match;
+
+    const resource = yield request(`https://mp3.zing.vn/xhr/media/get-source?type=audio&${matchUrl}`);
 
     const [matchUrl] = match;
     return { url: "https://mp3.zing.vn/xhr/media/get-source?type=audio&" + matchUrl };
